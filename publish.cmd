f601bb614d68c01e0324622cff046c25bd9787d6
siddhiwesanekar <90743476+siddhiwesanekar@users.noreply.github.com>
2022-01-05 11:19:35 +0530

    Delete publish.cmd

--- publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\smartbotplm-bot-be7a-Web-Deploy.pubxml -p:Password=c9oPncsHF159mxPrKvkWzrp7oXgnsREtDehn0xEDYfZagblyc9qpmMqrtYjk
-
