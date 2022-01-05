d4b7a38bf6d7631b443ee112f0aa3d1db2a20f08
siddhiwesanekar <90743476+siddhiwesanekar@users.noreply.github.com>
2022-01-05 11:05:12 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\smartbotplm-bot-be7a-Web-Deploy.pubxml -p:Password=c9oPncsHF159mxPrKvkWzrp7oXgnsREtDehn0xEDYfZagblyc9qpmMqrtYjk
+
